1b53515e62286793b9303a48d11fcad3557db4cd
Jonathan Barrios <jeb2239@columbia.edu>
2016-11-26 13:59:09 -0500

    added attribute parsing example,removed compatability lines from header

--- src/cil_compat.h
+++ src/cil_compat.h
@@ -2,8 +2,8 @@
 #define __cil_compat__
 
 // Get rid of some C language extensions that cil does not support.
-#define __attribute__(x)
-#define _Nonnull
-#define _Nullable
+// #define __attribute__(x)
+// #define _Nonnull
+// #define _Nullable
 
 #endif // __cil_compat__
